f5fbbfeaba0c4bf26d6e97f41872f8f42f7f8113
Md.Samiul Haque <samiulh4@gmail.com>
2022-06-14 00:54:11 +0600

    working on 34:41

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import Header from './components/Header'
 
 
 const App = () => {
   return (
-    <div><h1>Hello Wrold</h1></div>
-  )
+    <div className="container">
+        <Header/>
+    </div>
+  );
 }
 
 export default App
